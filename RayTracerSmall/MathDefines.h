81f70d19a7901d85715b666a2ae800695117955e
Josh <joshdgibson21@outlook.com>
2016-02-01 21:54:00 +0000

    Speperated code into different header files

---
+++ RayTracerSmall/MathDefines.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+// for OS specific defines
+
+#if defined __linux__ || defined __APPLE__
+// "Compiled for Linux
+#else
+// Windows doesn't define these values by default, Linux does
+#define M_PI 3.141592653589793
+#define INFINITY 1e8
+#endif
